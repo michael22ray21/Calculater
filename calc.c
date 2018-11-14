1d890f0ce6f3638f0aa9a0d764711beac61365e3
michael22ray21 <michael22ray21@yahoo.com>
2018-11-14 19:05:48 +0700

    Added Struct

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char oper;
+    double nilai;
+    int prio;
+} elmt;
+
+int main()
+{
+    /* KAMUS */
+    char ekspresi[101];
+
+    /* ALGORITMA */
+}

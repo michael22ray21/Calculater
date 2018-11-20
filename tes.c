0ad466e849cea5d1e16dec89fba26e6d6d58ccba
hilminaufal86 <hilminaufal86@users.noreply.github.com>
2018-11-20 10:03:21 +0700

    Fungsi input dan validasi
    Buat ngecek isi dari array yg kesimpan dan contoh makenya liat tes.c

---
+++ tes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "valinput.h"
+#include "boolean.h"
+
+int main() {
+Tabel T;
+boolean IsSyntaxTrue;
+
+InputUser(&T,&IsSyntaxTrue);
+if (IsSyntaxTrue) {
+  printf("Syntax Benar\n");
+  printf("NEff(T) = %d\n",NEff(T));
+  for (int i=1;i<=NEff(T);i++) {
+    printf ("%f  | %d   | %c \n",Nilai(T,i),Prio(T,i),Oper(T,i));
+  }
+}
+else {
+  printf("Syntax salah\n");
+}
+return 0;
+}
---
+++ valinput.h
@@ -0,0 +1,65 @@
+#ifndef _VALINPUT_h
+#define _VALINPUT_h
+
+#include "boolean.h"
+
+#define OperUndef '?'
+#define NilaiUndef -999
+#define MAXEL 100
+typedef float infotype;
+typedef struct
+{
+    char oper; /* Tempat menyimpan operator */
+    infotype nilai; /* Tempat menyimpan nilai real operan */
+    int prio; /* Prioritas operator */
+} elmt; /* Type elemen tabel */
+typedef struct
+{
+    elmt matArr[MAXEL+1]; /* Array */
+    int Neff; /* Banyaknya elemen efektif array */
+} Tabel; /* Type array */
+
+typedef char string[MAXEL+1];
+
+#define NEff(T) (T).Neff
+#define Oper(T, i) (T).matArr[i].oper
+#define Prio(T, i) (T).matArr[i].prio
+#define Nilai(T, i) (T).matArr[i].nilai
+
+/* fungsi2 */
+void CreateEmptyTabel (Tabel *T);
+/*
+membuat tabel kosong, Neff(T) = 0; */
+void InputUser(Tabel *T,boolean *IsSyntaxTrue);
+
+void InputToArray(string S, Tabel *T);
+
+boolean SyntaxValidity(string S,int i,boolean IsNeg,boolean IsFrac);
+
+boolean IsBalancedKurung(string S);
+
+boolean IsBlank(char C);
+
+boolean IsDigit(char C);
+
+boolean IsDot(char C);
+
+boolean IsNegSign(char C);
+
+boolean IsBukaKurung(char C);
+
+boolean IstutupKurung(char C);
+
+boolean IsOperator(char C);
+
+boolean IsKurung(char C);
+
+infotype charToInt(char C);
+
+int Priority(char C);
+
+
+
+/*mengembalikan true jika karakter yg dibaca adalah buka atau tutup kurung*/
+
+#endif
